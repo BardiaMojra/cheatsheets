deb389adffe1bb614098b393a4513dae7a6556c0
bardi <bardia.mojra@gmail.com>
2022-07-28 10:50:16 -0500

    Updated with git-express on 2022-07-28.

---
+++ scripts/fname_lint_ext.py
@@ -0,0 +1,36 @@
+
+import os
+srcDir = './'
+
+
+ext = '.pdf'
+print('\n\n\n rename files with extension: '+ext+'\n\n')
+# or
+#ext = int(input('\n enter file extension: '))
+files = list()
+
+for i, file in enumerate(os.listdir(srcDir)):
+  print(i, file)
+  if (os.path.isfile(srcDir+file) & file.endswith(ext)):
+    newfile = file
+    newfile = newfile.replace(' ', '_').lower()
+    newfile = newfile.replace('&', 'n')
+    newfile = newfile.replace(',', '')
+    newfile = newfile.replace('and', 'n')
+    newfile = newfile.replace("'", '')
+    newfile = newfile.replace('-', '_')
+    newfile = newfile.replace('__', '_')
+    newfile = newfile.replace('__', '_')
+    src = srcDir + file
+    files.append(src)
+    dest = srcDir + '_{:0>3d}_'.format(len(files)) + newfile
+    os.rename(src, dest)
+    #print(i, file)
+  else:
+    i = i-1
+
+print('files renamed... ')
+for i, file in enumerate(os.listdir(srcDir)):
+  print(i, file)
+
+# EOF
